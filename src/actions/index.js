dfa29b2381794e14558756c29d2bbe76d82d6f42
Mario de Biase <debiasej@gmail.com>
2016-12-10 19:53:02 +0100

    Add fetchWeather action and the reducers

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '14ddbab17a068f008ba371bf83d9ba69';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
